8c8cdecf19b76f8f870e416033e80c3dc69591ad
pipusana <Pipusana.p@admins-MacBook-Pro.local>
2016-10-27 10:05:09 +0700

    duplicate in production code

--- RomanNumber.php
+++ RomanNumber.php
@@ -4,13 +4,9 @@ class RomanNumber
 {
      public function convertToRomanNumber($number)
      {
-     	  if($number == 2){
-     	  	return 'II';
-     	  } else if ($number == 3){
-     	  	return 'III';
-     	  }
-          return 'I';
+          return str_repeat("I",$number);
      }
+
 }
 
 ?>
