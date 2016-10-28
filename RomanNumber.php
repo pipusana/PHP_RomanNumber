3f4725ba5889bd64c9dcd7bb228a3c56f94fc29b
pipusana <Pipusana.p@admins-MacBook-Pro.local>
2016-10-28 11:40:04 +0700

    duplicate in code Romannumber

--- RomanNumber.php
+++ RomanNumber.php
@@ -2,18 +2,17 @@
 
 class RomanNumber
 {
-    private $valueRomanNumber = ['X','I'];
-
+    private $valueRomanNumber = ['XL','X','IV','I'];
     public function convertToRomanNumber($number)
     {
     	$result = '';
         if($number >= 10){
           $number = $number/10;
           if($number == 4) {
-            $result = 'XL';
+            $result = $this->printToRomanNumeral(1,0);;
           }
           if($number < 4){
-            $result = $this->printToRomanNumeral_I_X($number,0);
+            $result = $this->printToRomanNumeral($number,1);
           }
           return $result;
         }
@@ -25,15 +24,15 @@ class RomanNumber
      		$result = 'V';
      	}
      	if($number == 4) {
-     		$result = 'IV';
+     		$result = $this->printToRomanNumeral(1,2);;
      	}
      	if($number < 4) {
-			$result .= $this->printToRomanNumeral_I_X($number,1);
+			$result .= $this->printToRomanNumeral($number,3);
      	}
         return $result;
     }
 
-    public function printToRomanNumeral_I_X($number,$index)
+    public function printToRomanNumeral($number,$index)
     {
         $result = '';
         return $result .= str_repeat($this->valueRomanNumber[$index],$number);
